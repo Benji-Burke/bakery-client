5a1718694e61f54ecab53f613fd354aabba1757c
Ben Burke <benjaminstevenburke@gmail.com>
2019-09-26 15:24:08 -0400

    rendering data need to figure out how to call the body

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Homepage from './components/Homepage';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import Carousel from './components/Carousel';
+import logo from './components/migdalias.png';
+import Footer from './components/Footer'
 
 class App extends Component {
     constructor(props) {
@@ -29,7 +32,7 @@ class App extends Component {
                 <div>
                     <Router>
         <Navbar color="faded" light>
-          <NavbarBrand to="/" className="mr-auto">Bakery</NavbarBrand>
+          <NavbarBrand to="/" className="mr-auto"><img className="logo" src={logo}/></NavbarBrand>
           <NavbarToggler onClick={this.toggle} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
@@ -50,7 +53,9 @@ class App extends Component {
                     <body>
                         <Route path="/" exact render={props => (
                             <Homepage/>
+                            
                         )}/>
+                        <Footer />
                     </body>
                 </Router>
             </div>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,23 +1,65 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Carousel from './Carousel';
+import Instagram from './Instagram';
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3003/bakery'
+
+
+
+
 
 class Homepage extends Component {
     constructor(props) {    
     super(props)
     this.state={
         picturesSlider: [],
-        pictures: []
+        pictures: [],
+        foundBio: [],
+       
     }
     }
+
+    componentDidMount(){
+        this.getAboutMe()
+    }
+
+    async getAboutMe(){
+        const response = await axios.get(`${baseURL}/about`)
+        const data = response.data;
+        console.log(response)
+        console.log(data)
+        this.setState({
+            foundBio: data,
+            // body: foundBio[0].body
+            
+        })
+    }
+
+    // getBody(body){
+    //     this.setState({
+    //         body: body,
+    //     })
+    // }
+
+   
 
     render() {
         return(
-            <div>
-            <h1>Homepage</h1>
+            <div >
+            {/* <h1>MigdaliasConfections</h1> */}
+            <img src=""/>
             <div className="carousel-container">
 
             <h1 className="pictures"><Carousel/></h1>
+            <div className="welcome">
+            <h1>Welcome to Migdalias Confections</h1>
+            <i className="fas fa-birthday-cake"></i>
+        
+            <p>{}</p>
+            </div>
+            <div><Instagram/></div>
             </div>
             </div>
             
