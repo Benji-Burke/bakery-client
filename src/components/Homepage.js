5e60d879455c43cb52de11bc0236a2f2f1723e0b
Zach Gordon <zachgordon@Zachs-MacBook-Pro.local>
2019-09-26 16:22:21 -0400

    rendered about

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,76 +1,68 @@
-import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Carousel from './Carousel';
 
 import Instagram from './Instagram';
-import axios from 'axios'
+import axios from 'axios';
 
-const baseURL = 'http://localhost:3003/bakery'
-
-
-
-
+// const baseURL = 'http://localhost:3003/bakery'
+const baseURL = 'https://migdalias-confections.herokuapp.com/bakery';
 
 class Homepage extends Component {
-    constructor(props) {    
-    super(props)
-    this.state={
-        picturesSlider: [],
-        pictures: [],
-        foundBio: [],
-       
-    }
-    this.getAboutMe=this.getAboutMe.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      picturesSlider: [],
+      pictures: [],
+      foundBio: []
+    };
+    this.getAboutMe = this.getAboutMe.bind(this);
+  }
 
-    componentDidMount(){
-        this.getAboutMe()
-    }
+  componentDidMount() {
+    this.getAboutMe();
+  }
 
-    async getAboutMe(){
-        const response = await axios.get(`${baseURL}/about`)
-        const data = response.data;
-        console.log(response)
-        console.log(data.foundBio[0].body)
-        this.setState({
-                foundBio: data,
-                body: data.foundBio[0].body
+  async getAboutMe() {
+    const response = await axios.get(`${baseURL}/about`);
+    const data = response.data;
+    // console.log(response);
+    console.log(data.foundBio[0].body);
+    this.setState({
+      foundBio: data,
+      body: data.foundBio[0].body
+    });
+  }
 
-            
-            
-        })
-    }
+  // getBody(body){
+  //     this.setState({
+  //         body: body,
+  //     })
+  // }
 
-    // getBody(body){
-    //     this.setState({
-    //         body: body,
-    //     })
-    // }
-
-   
-
-    render() {
-        return(
-
-            <div >
-            {/* <h1>MigdaliasConfections</h1> */}
-            <img src=""/>
-            <div className="carousel-container">
-
-            <h1 className="pictures"><Carousel/></h1>
-            <div className="welcome">
+  render() {
+    return (
+      <div>
+        {/* <h1>MigdaliasConfections</h1> */}
+        <img src='' />
+        <div className='carousel-container'>
+          <h1 className='pictures'>
+            <Carousel />
+          </h1>
+          <div className='welcome'>
             <h1>Welcome to Migdalias Confections</h1>
-            <i className="fas fa-birthday-cake"></i>
-        
-            <p>{}</p>
-            </div>
-            <div><Instagram/></div>
+            <p>{this.state.body}</p>
+            <i className='fas fa-birthday-cake'></i>
 
-            </div>
-            </div>
-            
-        )
-    }
+            <p>{}</p>
+          </div>
+          <div>
+            <Instagram />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
