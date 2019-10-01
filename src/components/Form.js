93181137df096d743fdfae0aa44e5feefde6e621
Gary Beaumont <garyallenbeaumont@gmail.com>
2019-10-01 11:27:45 -0400

    got form to console log data and store in state

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,8 +7,58 @@ class ContactForm extends Component {
     super(props)
     this.state={
         picturesSlider: [],
-        pictures: []
+        pictures: [],
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+
     }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleNameChange = this.handleNameChange.bind(this)
+    this.handleEmailChange = this.handleEmailChange.bind(this)
+    this.handlePhoneChange = this.handlePhoneChange.bind(this)
+    this.handleMessageChange = this.handleMessageChange.bind(this)
+    }
+
+    async handleNameChange(event) {
+        event.preventDefault()
+        let nameInput = event.target.value;
+        await this.setState({
+            name: nameInput
+        })
+        
+    }
+
+    async handleEmailChange(event) {
+        event.preventDefault()
+        let eamilInput = event.target.value;
+        await this.setState({
+            email: eamilInput
+        })
+        
+    }
+
+    async handlePhoneChange(event) {
+        event.preventDefault()
+        let phoneInput = event.target.value;
+        await this.setState({
+            phone: phoneInput
+        })
+        
+    }
+
+    async handleMessageChange(event) {
+        event.preventDefault()
+        let messageInput = event.target.value;
+        await this.setState({
+            message: messageInput
+        })
+        
+    }
+
+    handleClick() {
+        console.log(this.state.name, this.state.email, this.state.phone, this.state.message)
     }
 
     render() {
@@ -17,21 +67,25 @@ class ContactForm extends Component {
                 <Form>
                     <FormGroup>
                         <Label for="name">Name</Label>
-                        <Input type="text" name="name" id="name" placeholder="Enter name here" required></Input>
+                        <Input type="text" name="name" id="name" value={this.state.name} onChange={this.handleNameChange} placeholder="Enter name here" required></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="email">Email</Label>
-                        <Input type="email" id="email" placeholder="Enter email here" required></Input>
+                        <Input type="email" id="email" value={this.state.email} onChange={this.handleEmailChange}placeholder="Enter email here" required></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="phone">Phone</Label>
-                        <Input type="tel" id="phone" placeholder="Enter phone number here" required></Input>
+                        <Input type="tel" id="phone" value={this.state.phone} onChange={this.handlePhoneChange}placeholder="Enter phone number here" required></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label for="text">Message</Label>
-                        <Input type="textarea" id="text" placeholder="Enter message here" required></Input>
+                        <Input type="textarea" id="text" value={this.state.message} onChange={this.handleMessageChange}placeholder="Enter message here" required></Input>
                     </FormGroup>
-                    <Button>Submit</Button>
+                    <Button
+                        onClick={() => {
+                            this.handleClick()
+                        }}
+                    >Submit</Button>
                 </Form>
             </div>
             
